efc577278797f3c94683a18c81f7d29c477f26b4
Eric Lamoureux <ericandbailey@gmail.com>
2025-11-07 10:40:20 -0500

    Fixed Enemy Sprite Pivot Points

---
+++ Assets/Notes.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Notes
+{
+    
+    // Move shooting logic into EnemyAttack script
+    // Setup an Enum in EnemyController
+    // Make enemy turn in shoot direction
+    // Fix enemy pivot point
+
+
+    // Scriptable objects
+
+
+
+
+
+}
